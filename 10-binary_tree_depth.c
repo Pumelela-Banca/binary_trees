82c000ab32c382a1e027530f1c83b4c285e4c92b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-31 00:50:47 +0200

    added depth function and prototype

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a
+ * node in a binary tree
+ *
+ * @tree: pointer to node
+ *
+ * Return:  depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	while (tree->parent != NULL)
+	{
+		tree = tree->parent;
+		count++;
+	}
+	return (count);
+}
