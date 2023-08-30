e3443b6621a5b8e9976d2b2d12099a1fb6fa99d7
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-30 11:02:57 +0200

    added post-order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through a binary tree using
+ * in-order traversal
+ *
+ * @tree: pointer to the root node of the tree
+ * @func: to a function to call for each node
+ *
+ * Return: void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
+}
