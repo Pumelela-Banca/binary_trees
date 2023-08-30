cf163186395f91460349ff064a072d3fedcf3fda
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-30 03:53:06 +0200

    adds node to left of tree

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t - insert node at left
+ *
+ * @parent: parent node
+ * @value value
+ *
+ * Return: new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL || parent == NULL)
+		return (NULL);
+	if (parent->left == NULL)
+	{
+		parent->left = new;
+		new->left = NULL;
+	}
+	else
+	{
+		new->left = parent->left;
+		parent->left = new;
+	}
+	new->right = NULL;
+	new->n = value;
+	return (new);
+}
