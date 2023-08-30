0dbc9602dad20fd55479550ed2813077ae3d95ab
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-30 04:07:45 +0200

    added left is turned to null

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right = new;
 		new->right->parent = new;
 	}
-	new->right = NULL;
+	new->left = NULL;
 	new->parent = parent;
 	new->n = value;
 	return (new);
