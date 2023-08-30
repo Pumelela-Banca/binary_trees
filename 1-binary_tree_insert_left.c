51d18eb5d38ace06b6aa86e3c0a31df8da2b0153
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-30 03:58:54 +0200

    added parent to new and old nodes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - insert node at left
+ * binary_tree_insert_left - insert node at left
  *
  * @parent: parent node
- * @value value
+ * @value: value
  *
  * Return: new node
  */
@@ -25,8 +25,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		new->left = parent->left;
 		parent->left = new;
+		new->left->parent = new;
 	}
 	new->right = NULL;
+	new->parent = parent;
 	new->n = value;
 	return (new);
 }
