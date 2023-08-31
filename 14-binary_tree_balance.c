b6f0fe891abc40a125bcda8cb0d8ecf90e177efd
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-31 15:22:42 +0200

    added conditions for single node

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,6 +12,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	else if (tree->left == NULL)
+		return (tree_height(tree->right) * -1);
+	else if (tree->right == NULL)
+		return (tree_height(tree->left));
 	return (tree_height(tree->left) -
 			tree_height(tree->right));
 }
@@ -30,7 +37,7 @@ size_t tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL ||
 			(!tree->left && !tree->right))
-		return (0);
+		return (1);
 
 	lef = tree_height(tree->left);
 	rig = tree_height(tree->right);
