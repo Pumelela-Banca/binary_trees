743a2d3ac2799bab7c7b7f3b2441ed9ee25e402c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-01 03:51:42 +0200

    added condition for no parent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,15 +10,14 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (tree == NULL)
+	if (node == NULL)
 		return (NULL);
-	if (node->parent != NULL &&
-			node->parent->parent->left == node->parent
-			&& node->parent->parent->right)
-		return (node->parent->parent->right);
-	else if (node->parent != NULL &&
-			node->parent->parent->right == node->parent
-			&& node->parent->parent->left)
+	if (node->parent == NULL ||
+			node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent != NULL && node == node->parent->left)
+		return (node->parent->parent->left);
+	else if (node->parent != NULL && node == node->parent->right)
 		return (node->parent->parent->right);
 	else
 		return (NULL);
