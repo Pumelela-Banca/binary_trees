bb156ad2ece5f6aeaece4ba46694b4fb102d8e37
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-01 03:17:56 +0200

    added empty file

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - ckecks if tree is  perfect
+ *
+ * @tree: node to check
+ *
+ * Return: 1 or 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+}
